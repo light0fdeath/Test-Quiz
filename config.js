dd9669c126e9e69e423bc9c50b218ce243d274a4
Bigyan Khanal <bigyan.khanal7@gmail.com>
2017-10-30 22:06:52 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,60 @@
+
+  var configuration = {
+   "tile1": {
+    "title": "Human Error",
+    "parts": [{
+     "title": "Disciplining",
+     "feedback": "Not quite! Disciplining of the individual involved is not the best response for a Human Error.Try again."
+    }, {
+     "title": "Remediation",
+     "feedback": "Not quite! Implementing a new remedy is not the best response for a Human Error."
+    }, {
+     "title": "Acceptance",
+     "feedback": "Well done! A Human Error should be accepted as a natural and common part of work. No further analysis or corrective actions are necessary with this type of error."
+    }]
+   },
+
+   "tile2": {
+    "title": "Avoidable Error",
+    "parts": [{
+     "title": "Disciplining",
+     "feedback": "Not quite! Disciplining of the individual involved is not the best response for an Avoidable Error."
+    }, {
+     "title": "Remediation",
+     "feedback": "Well done! An Avoidable Error can be prevented from happening again through management analysis and putting a remedy in place to fix the problem."
+    }, {
+     "title": "Acceptance",
+     "feedback": "Not quite! Acceptance is not the best response for an Avoidable Error."
+    }]
+   },
+
+   "tile3": {
+    "title": "‘Due Care’ Violation",
+    "parts": [{
+     "title": "Disciplining",
+     "feedback": "Not quite! Disciplining of the individual involved is not the best response for a ‘Due Care’ Violation."
+    }, {
+     "title": "Remediation",
+     "feedback": "Well done! Remediation and management analysis are the best response to a ‘Due Care’ Violation. A similar mistake could be prevented in the future by taking corrective action such as modified processes or additional training."
+    }, {
+     "title": "Acceptance",
+     "feedback": "Not quite! Acceptance is not the best response for a ‘Due Care’ Violation."
+    }]
+   },
+
+   "tile4": {
+    "title": "Willful Misconduct",
+    "parts": [{
+     "title": "Disciplining",
+     "feedback": "Well done! When a mistake is caused by an individual purposefully committing a course of action they know is wrong, disciplining, along with management analysis, is the most appropriate response."
+    }, {
+     "title": "Remediation",
+     "feedback": "Not quite! Implementing a new remedy is not the best response to Willful Misconduct."
+    }, {
+     "title": "Acceptance",
+     "feedback": "Not quite! Acceptance is not the best response to Willful Misconduct."
+    }]
+    }
+   
+
+  }
