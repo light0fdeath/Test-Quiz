7ba62d36e7faebaa11394284a30bed6d1931f0fb
Bigyan Khanal <bigyan.khanal7@gmail.com>
2017-11-04 16:17:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,13 +4,16 @@
     "title": "Human Error",
     "parts": [{
      "title": "Disciplining",
-     "feedback": "Not quite! Disciplining of the individual involved is not the best response for a Human Error.Try again."
+     "feedback": "Not quite! Disciplining of the individual involved is not the best response for a Human Error.Try again.",
+     "answer":"incorrect"
     }, {
      "title": "Remediation",
-     "feedback": "Not quite! Implementing a new remedy is not the best response for a Human Error."
+     "feedback": "Not quite! Implementing a new remedy is not the best response for a Human Error.",
+     "answer":"incorrect"
     }, {
      "title": "Acceptance",
-     "feedback": "Well done! A Human Error should be accepted as a natural and common part of work. No further analysis or corrective actions are necessary with this type of error."
+     "feedback": "Well done! A Human Error should be accepted as a natural and common part of work. No further analysis or corrective actions are necessary with this type of error.",
+     "answer":"correct"
     }]
    },
 
@@ -18,13 +21,16 @@
     "title": "Avoidable Error",
     "parts": [{
      "title": "a",
-     "feedback": "Not quite! Disciplining of the individual involved is not the best response for an Avoidable Error."
+     "feedback": "Not quite! Disciplining of the individual involved is not the best response for an Avoidable Error.",
+     "answer":"incorrect"
     }, {
      "title": "fghfhfghgfh",
-     "feedback": "Well done! An Avoidable Error can be prevented from happening again through management analysis and putting a remedy in place to fix the problem."
+     "feedback": "Well done! An Avoidable Error can be prevented from happening again through management analysis and putting a remedy in place to fix the problem.",
+     "answer":"correct"
     }, {
      "title": "fghgfh",
-     "feedback": "Not quite! Acceptance is not the best response for an Avoidable Error."
+     "feedback": "Not quite! Acceptance is not the best response for an Avoidable Error.",
+     "answer":"incorrect"
     }]
    },
 
@@ -32,13 +38,16 @@
     "title": "'Due Care' Violation",
     "parts": [{
      "title": "dsf",
-     "feedback": "Not quite! Disciplining of the individual involved is not the best response for a ‘Due Care’ Violation."
+     "feedback": "Not quite! Disciplining of the individual involved is not the best response for a ‘Due Care’ Violation.",
+     "answer":"incorrect"
     }, {
      "title": "sd",
-     "feedback": "Well done! Remediation and management analysis are the best response to a ‘Due Care’ Violation. A similar mistake could be prevented in the future by taking corrective action such as modified processes or additional training."
+     "feedback": "Well done! Remediation and management analysis are the best response to a ‘Due Care’ Violation. A similar mistake could be prevented in the future by taking corrective action such as modified processes or additional training.",
+     "answer":"correct"
     }, {
      "title": "as",
-     "feedback": "Not quite! Acceptance is not the best response for a ‘Due Care’ Violation."
+     "feedback": "Not quite! Acceptance is not the best response for a ‘Due Care’ Violation.",
+     "answer":"incorrect"
     }]
    },
 
@@ -46,13 +55,16 @@
     "title": "Willful Misconduct",
     "parts": [{
      "title": "T",
-     "feedback": "Well done! When a mistake is caused by an individual purposefully committing a course of action they know is wrong, disciplining, along with management analysis, is the most appropriate response."
+     "feedback": "Well done! When a mistake is caused by an individual purposefully committing a course of action they know is wrong, disciplining, along with management analysis, is the most appropriate response.",
+     "answer":"correct"
     }, {
      "title": "b",
-     "feedback": "Not quite! Implementing a new remedy is not the best response to Willful Misconduct."
+     "feedback": "Not quite! Implementing a new remedy is not the best response to Willful Misconduct.",
+     "answer":"incorrect"
     }, {
      "title": "c",
-     "feedback": "Not quite! Acceptance is not the best response to Willful Misconduct."
+     "feedback": "Not quite! Acceptance is not the best response to Willful Misconduct.",
+     "answer":"incorrect"
     }]
     }
    
